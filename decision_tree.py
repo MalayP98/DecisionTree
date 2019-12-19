7435a142cad45621e52a83205b6fccdaf51894e4
malay <tomalaypandey.tanu@gmail.com>
2019-12-19 19:54:18 +0530

    Structure

--- decision_tree.py
+++ decision_tree.py
@@ -1,1 +1,25 @@
+import numpy as np
+from sklearn import datasets
+
+dataset = datasets.load_iris()
+
+
+class DecisionTree:
+    def __init__(self, training_data, impurity_measure = 'entropy'):
+        pass
+
+    def purity(self):
+        pass
+
+    def split(self):
+        pass
+
+    def entropy(self):
+        pass
+
+    def total_entropy(self):
+        pass
+
+    def grow(self):
+        pass
 
