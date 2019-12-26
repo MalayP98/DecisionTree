740159bef76adc2756e22b6fddc923b093596da0
malay <tomalaypandey.tanu@gmail.com>
2019-12-26 08:56:38 +0530

    Purity function done

--- decision_tree.py
+++ decision_tree.py
@@ -2,19 +2,65 @@ import numpy as np
 from sklearn import datasets
 
 dataset = datasets.load_iris()
+x = dataset.data
+y = dataset.target
+
+
+def add_indices(data):
+    new_data = []
+    rows = data.shape[0]
+    for i in range(rows):
+        sample = np.append(i, data[i])
+        new_data.append(list(sample))
+
+    print(new_data)
+    return np.array(new_data)
+
+
+x = add_indices(x)
+
+
+def train_test_split(input, target, ratio):
+    shuffle_indices = np.random.permutation(input.shape[0])
+    test_size = int(input.shape[0] * ratio)
+    train_indices = shuffle_indices[:test_size]
+    test_indices = shuffle_indices[test_size:]
+    xTrain = input[train_indices]
+    yTrain = target[train_indices]
+    xTest = input[test_indices]
+    yTest = target[test_indices]
+    return xTrain, yTrain, xTest, yTest
+
+
+xTrain, yTrain, xTest, yTest = train_test_split(x, y, 0.8)
 
 
 class DecisionTree:
-    def __init__(self, training_data, impurity_measure = 'entropy'):
+    def __init__(self, training_data, target, impurity_measure='entropy'):
+        self.data = training_data
+        self.impurity_measure = impurity_measure
+        self.target = target
+        print(self.purity(training_data))
+
+    def purity(self, data):
+        diff_classes = {}
+        self.data = data
+        indices = data[:, 0]
+        class_in_sample = [self.target[int(i)] for i in indices]
+        for i in range(len(class_in_sample)):
+            if i == 0:
+                diff_classes[class_in_sample[i]] = 1
+            if class_in_sample[i] in diff_classes.keys():
+                diff_classes[class_in_sample[i]] += 1
+            else:
+                return 0
+
+        return 1
+
+    def split(self, data, column, value):
         pass
 
-    def purity(self):
-        pass
-
-    def split(self):
-        pass
-
-    def entropy(self):
+    def entropy(self, data):
         pass
 
     def total_entropy(self):
@@ -23,3 +69,7 @@ class DecisionTree:
     def grow(self):
         pass
 
+
+obj = DecisionTree(x, y)
+c1, c2 = obj.split(x, 2, 3)
+en = obj.entropy(x)
